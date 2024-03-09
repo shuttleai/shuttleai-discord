48886bb5fb4c72a027e3f670c7c22335dbd2711d
victim <149972831+alluding@users.noreply.github.com>
2024-03-08 19:05:48 -0500

    Create __init__.py

---
+++ src/shuttleai/models/__init__.py
@@ -0,0 +1,21 @@
+from ._models import (
+    Item,
+    Image,
+    Audio,
+    Message,
+    Choice,
+    Usage,
+    Chat,
+    Embedding
+)
+
+slots = [
+    "Item",
+    "Image",
+    "Audio",
+    "Message",
+    "Choice",
+    "Usage",
+    "Chat",
+    "Embedding"
+]
