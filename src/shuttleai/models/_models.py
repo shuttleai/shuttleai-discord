71729dc8295e9b568f5b6241d02ff9e29ea3ef28
Hermes Trismegistus <91907013+herumes@users.noreply.github.com>
2024-03-16 20:54:51 -0700

    Add streaming responses

--- src/shuttleai/models/_models.py
+++ src/shuttleai/models/_models.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any, Optional
+from typing import List, Any, Optional
 from pydantic import BaseModel
 
 class Item(BaseModel):
@@ -18,16 +18,32 @@ class Message(BaseModel):
     content: str
     role: str
 
+class Delta(BaseModel):
+    content: str
+
 class Choice(BaseModel):
     finish_reason: str
     index: int
     # logprobs: Optional[Dict[str, Any]]
     message: Message
 
+class StreamChoice(BaseModel):
+    finish_reason: Optional[str] | None
+    index: int 
+    # logprobs: Optional[Dict[str, Any]]
+    delta: Delta
+
 class Usage(BaseModel):
     completion_tokens: int
     prompt_tokens: int
     total_tokens: int
+
+class ChatChunk(BaseModel):
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: List[StreamChoice]
 
 class Chat(BaseModel):
     id: str
