f69221fc62b65dd0c1d4c5d533169f1e8e7dcbd9
Hermes Trismegistus <chris@shuttleai.app>
2024-06-27 15:22:29 -0700

    Update chat_with_image.py

--- examples/chat_with_image.py
+++ examples/chat_with_image.py
@@ -2,7 +2,7 @@
 
 
 from shuttleai import ShuttleAI
-from shuttleai.schemas.chat.completions import (  # Helper for messages
+from shuttleai.schemas.chat.completions import (  # Helper for messages, not required!
     ChatMessage,
     ChatMessageContentPartImage,  # For v5 use
     ChatMessageContentPartText,  # For v5 use
