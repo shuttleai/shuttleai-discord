b83629de2995b67145fa8b61a1f6542301888f5d
Hermes Trismegistus <herumes11@gmail.com>
2024-08-24 01:35:22 -0700

    Fix failing branch

--- shuttleai/schemas/chat/completions.py
+++ shuttleai/schemas/chat/completions.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from typing import Annotated, List, Optional, Union
+from typing import List, Optional, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from shuttleai.exceptions import ShuttleAIException
 from shuttleai.schemas.common import UsageInfo
