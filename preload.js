caa55c24b5c08fe514c5e50e4b27afb8c45d6d44
tristandevs <113070063+tristandevs@users.noreply.github.com>
2024-10-14 16:18:08 -0400

    auto update and more

--- preload.js
+++ preload.js
@@ -4,4 +4,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.send('minimize-window'),
   maximizeWindow: () => ipcRenderer.send('maximize-window'),
   closeWindow: () => ipcRenderer.send('close-window'),
+
+  onUpdateAvailable: (callback) => ipcRenderer.on('update_available', callback),
+  onUpdateDownloaded: (callback) => ipcRenderer.on('update_downloaded', callback),
+
+  onProgressUpdate: (callback) => ipcRenderer.on('download_progress', callback)
 });
