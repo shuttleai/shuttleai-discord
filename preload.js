e90799b8e665717d75836f20d1152fac915f1944
tristandevs <113070063+tristandevs@users.noreply.github.com>
2024-10-14 14:55:17 -0400

    Initial Commit

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimizeWindow: () => ipcRenderer.send('minimize-window'),
+  maximizeWindow: () => ipcRenderer.send('maximize-window'),
+  closeWindow: () => ipcRenderer.send('close-window'),
+});
